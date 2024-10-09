6e68dca6e6a2f103144bb8a6939b7ff04cc931e9
李继龙 <kljzndx@outlook.com>
2024-10-09 09:17:19 +0800

    完成编写 parseDualPing 方法

---
+++ src/models/dualping.js
@@ -0,0 +1,63 @@
+/**
+ * @param {object} format 
+ * @param {string} format.[key] 
+ * @param {string} fullPing 
+ */
+function parseDualPing(format, fullPing) {
+    if (fullPing.length == 1) {
+        return {
+            left: { dualCode: fullPing, fullCode: fullPing },
+            right: { dualCode: fullPing, fullCode: fullPing },
+        };
+    }
+
+    if (fullPing.length == 2) {
+        const pLeft = fullPing[0];
+        const pRight = fullPing[1];
+        return {
+            left: { dualCode: pLeft, fullCode: pLeft },
+            right: { dualCode: pRight, fullCode: pRight },
+        };
+    }
+
+    let lDual = "";
+    let lFull = "";
+
+    let rDual = "";
+    let rFull = "";
+
+    for (const yingJie in format) {
+        if (!format.hasOwnProperty(yingJie)) continue;
+
+        /**
+         * @type {string}
+         */
+        const key = format[yingJie];
+
+        if (['ch', 'sh', 'zh'].includes(yingJie)) {
+            if (fullPing.startsWith(yingJie)) {
+                lDual = key;
+                lFull = yingJie;
+            }
+        } else {
+            if (fullPing.endsWith(yingJie)) {
+                rDual = key;
+                rFull = yingJie;
+            }
+        }
+
+        if (lDual != "" && rDual != "") break;
+    }
+
+    if (lDual == "") {
+        lDual = fullPing[0];
+        lFull = fullPing[0];
+    }
+
+    return {
+        left: { dualCode: lDual, fullCode: lFull },
+        right: { dualCode: rDual, fullCode: rFull },
+    };
+}
+
+export default { parseDualPing };
